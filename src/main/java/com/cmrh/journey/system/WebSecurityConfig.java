233bb518a85eb76d5921d6f8c835083fcbcbb051
ex-pangb001 <pbh.3644>
2018-08-28 17:07:02 +0800

    Spring Cloud eureka 高可用集群注冊中心

---
+++ src/main/java/com/cmrh/journey/system/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.cmrh.journey.system;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author pangbohuan
+ * @description Spring Security 默认开启了所有 CSRF 攻击防御，需要禁用 /eureka 的防御。
+ * @date 2018-08-21 10:52
+ **/
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**").and().authorizeRequests().anyRequest()
+                .authenticated().and().httpBasic();
+    }
+}
